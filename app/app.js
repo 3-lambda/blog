e967049952cb8d4ab39011f8961bdf4eaa7722a9
Ana <ana.bastos881@gmail.com>
2017-01-26 04:21:34 -0200

    Adicionando manifest.json e ajustes no nav bar

--- app/app.js
+++ app/app.js
@@ -11,6 +11,12 @@ angular.module('app', [
 	}).when('/leads', {
 		templateUrl: 'views/leads.html',
 		controller: 'LeadController'
+	}).when('/contato', {
+		templateUrl: 'views/contato.html',
+		controller: 'LeadController'
+	}).when('/quem-somos', {
+		templateUrl: 'views/quem-somos.html',
+		controller: 'LeadController'
 	}).otherwise({
 		redirectTo: '/'
 	});
